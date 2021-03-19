e7b00157ae71d2bf84914b81f8d44cf015d4152d
SORNAY Jean-Francois <Jean-Francois.Sornay@bordeaux-inp.fr>
2021-03-19 17:20:12 +0100

    Adding a compare function of color dict in helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -2,4 +2,12 @@ const getColor = function (_red, _green, _blue, _alpha) {
     return {red : _red, green : _green, blue : _blue, alpha : _alpha};
 }
 
+const compareColor = function (color1, color2) {
+    return (color1.red === color2.red
+        && color1.green === color2.green
+        && color1.blue === color2.blue
+        && color2.alpha === color2.alpha);
+}
+
 exports.getColor = getColor;
+exports.compareColor = compareColor;
