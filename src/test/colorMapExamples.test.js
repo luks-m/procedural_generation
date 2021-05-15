a1ab432e98578f88ee5b05fcb829c79a957b5ad4
SORNAY Jean-Francois <Jean-Francois.Sornay@bordeaux-inp.fr>
2021-05-15 15:26:56 +0200

    Fix typo <colorMapExamples.test|colorMapGenerator.test|colorMapPredicate.test>: Fix mixing tabs/spaces in the different file

--- src/test/colorMapExamples.test.js
+++ src/test/colorMapExamples.test.js
@@ -5,137 +5,137 @@ function testColorMapExamples() {
     describe('ColorMapExamples tests', () => {
 
         test('Test of colorMapGreys', () => {
-	    const gradient = colorMap.greys({f:(x,y)=>x,min:0,max:50});
-	    let color1 = gradient(0,5);
-	    expect(colors.compareColor(color1,colors.createColor(255,255,255,255))).toEqual(true);
-            let color2 = gradient(25,5);
-	    expect(colors.compareColor(color2,colors.createColor(128,128,128,255))).toEqual(true);
-	    let color3 = gradient(50,5);
-	    expect(colors.compareColor(color3,colors.createColor(2,2,2,255))).toEqual(true);
-	    let color4 = gradient(125,5);
-	    expect(colors.compareColor(color4,colors.createColor(131,131,131,255))).toEqual(true); 
-	});
+            const gradient = colorMap.greys({ f: (x, y) => x, min: 0, max: 50 });
+            let color1 = gradient(0, 5);
+            expect(colors.compareColor(color1, colors.createColor(255, 255, 255, 255))).toEqual(true);
+            let color2 = gradient(25, 5);
+            expect(colors.compareColor(color2, colors.createColor(128, 128, 128, 255))).toEqual(true);
+            let color3 = gradient(50, 5);
+            expect(colors.compareColor(color3, colors.createColor(2, 2, 2, 255))).toEqual(true);
+            let color4 = gradient(125, 5);
+            expect(colors.compareColor(color4, colors.createColor(131, 131, 131, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapMushroom', () => {
-	    const gradient = colorMap.mushroom({f:(x,y)=>x,min:0,max:100});
-	    let color1 = gradient(0,5);
-	    expect(colors.compareColor(color1,colors.createColor(255,0,0,255))).toEqual(true);
-            let color2 = gradient(50,5);
-	    expect(colors.compareColor(color2,colors.createColor(128,126,126,255))).toEqual(true);
-	    let color3 = gradient(100,5);
-	    expect(colors.compareColor(color3,colors.createColor(2,252,252,255))).toEqual(true);
-	    let color4 = gradient(250,5);
-	    expect(colors.compareColor(color4,colors.createColor(131,123,123,255))).toEqual(true); 
-	});
+        test('Test of colorMapMushroom', () => {
+            const gradient = colorMap.mushroom({ f: (x, y) => x, min: 0, max: 100 });
+            let color1 = gradient(0, 5);
+            expect(colors.compareColor(color1, colors.createColor(255, 0, 0, 255))).toEqual(true);
+            let color2 = gradient(50, 5);
+            expect(colors.compareColor(color2, colors.createColor(128, 126, 126, 255))).toEqual(true);
+            let color3 = gradient(100, 5);
+            expect(colors.compareColor(color3, colors.createColor(2, 252, 252, 255))).toEqual(true);
+            let color4 = gradient(250, 5);
+            expect(colors.compareColor(color4, colors.createColor(131, 123, 123, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapSpring', () => {
-	    const gradient = colorMap.spring({f:(x,y)=>x,min:10,max:0});
-	    let color1 = gradient(0,5);
-	    expect(colors.compareColor(color1,colors.createColor(2,252,0,255))).toEqual(true);
-            let color2 = gradient(10,5);
-	    expect(colors.compareColor(color2,colors.createColor(255,0,0,255))).toEqual(true);
-	    let color3 = gradient(5,5);
-	    expect(colors.compareColor(color3,colors.createColor(128,126,0,255))).toEqual(true);
-	    let color4 = gradient(25,5);
-	    expect(colors.compareColor(color4,colors.createColor(123,131,0,255))).toEqual(true); 
-	});
+        test('Test of colorMapSpring', () => {
+            const gradient = colorMap.spring({ f: (x, y) => x, min: 10, max: 0 });
+            let color1 = gradient(0, 5);
+            expect(colors.compareColor(color1, colors.createColor(2, 252, 0, 255))).toEqual(true);
+            let color2 = gradient(10, 5);
+            expect(colors.compareColor(color2, colors.createColor(255, 0, 0, 255))).toEqual(true);
+            let color3 = gradient(5, 5);
+            expect(colors.compareColor(color3, colors.createColor(128, 126, 0, 255))).toEqual(true);
+            let color4 = gradient(25, 5);
+            expect(colors.compareColor(color4, colors.createColor(123, 131, 0, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapJet', () => {
-	    const gradient = colorMap.jet({f:(x,y)=>y,min:25,max:5});
-	    let color1 = gradient(0,5);
-	    expect(colors.compareColor(color1,colors.createColor(2,0,252,255))).toEqual(true);
-            let color2 = gradient(10,25);
-	    expect(colors.compareColor(color2,colors.createColor(255,0,0,255))).toEqual(true);
-	    let color3 = gradient(5,15);
-	    expect(colors.compareColor(color3,colors.createColor(128,0,126,255))).toEqual(true);
-	    let color4 = gradient(25,55);
-	    expect(colors.compareColor(color4,colors.createColor(123,0,131,255))).toEqual(true); 
-	});
+        test('Test of colorMapJet', () => {
+            const gradient = colorMap.jet({ f: (x, y) => y, min: 25, max: 5 });
+            let color1 = gradient(0, 5);
+            expect(colors.compareColor(color1, colors.createColor(2, 0, 252, 255))).toEqual(true);
+            let color2 = gradient(10, 25);
+            expect(colors.compareColor(color2, colors.createColor(255, 0, 0, 255))).toEqual(true);
+            let color3 = gradient(5, 15);
+            expect(colors.compareColor(color3, colors.createColor(128, 0, 126, 255))).toEqual(true);
+            let color4 = gradient(25, 55);
+            expect(colors.compareColor(color4, colors.createColor(123, 0, 131, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapHSL', () => {
-	    const gradient = colorMap.hsl({f:(x,y)=>y,min:0,max:30});
-	    let color1 = gradient(0,0);
-	    expect(colors.compareColor(color1,colors.createColor(240,0,97,255))).toEqual(true);
-            let color2 = gradient(10,30);
-	    expect(colors.compareColor(color2,colors.createColor(240,252,97,255))).toEqual(true);
-	    let color3 = gradient(5,15);
-	    expect(colors.compareColor(color3,colors.createColor(240,126,97,255))).toEqual(true);
-	    let color4 = gradient(25,75);
-	    expect(colors.compareColor(color4,colors.createColor(240,123,97,255))).toEqual(true); 
-	});
+        test('Test of colorMapHSL', () => {
+            const gradient = colorMap.hsl({ f: (x, y) => y, min: 0, max: 30 });
+            let color1 = gradient(0, 0);
+            expect(colors.compareColor(color1, colors.createColor(240, 0, 97, 255))).toEqual(true);
+            let color2 = gradient(10, 30);
+            expect(colors.compareColor(color2, colors.createColor(240, 252, 97, 255))).toEqual(true);
+            let color3 = gradient(5, 15);
+            expect(colors.compareColor(color3, colors.createColor(240, 126, 97, 255))).toEqual(true);
+            let color4 = gradient(25, 75);
+            expect(colors.compareColor(color4, colors.createColor(240, 123, 97, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapLight', () => {
-	    const gradient = colorMap.light({f:(x,y)=>y,min:0,max:60});
-	    let color1 = gradient(0,0);
-	    expect(colors.compareColor(color1,colors.createColor(0,0,255,255))).toEqual(true);
-            let color2 = gradient(10,60);
-	    expect(colors.compareColor(color2,colors.createColor(6,0,0,255))).toEqual(true);
-	    let color3 = gradient(5,30);
-	    expect(colors.compareColor(color3,colors.createColor(59,193,0,255))).toEqual(true);
-	    let color4 = gradient(25,75);
-	    expect(colors.compareColor(color4,colors.createColor(0,155,95,255))).toEqual(true); 
-	});
+        test('Test of colorMapLight', () => {
+            const gradient = colorMap.light({ f: (x, y) => y, min: 0, max: 60 });
+            let color1 = gradient(0, 0);
+            expect(colors.compareColor(color1, colors.createColor(0, 0, 255, 255))).toEqual(true);
+            let color2 = gradient(10, 60);
+            expect(colors.compareColor(color2, colors.createColor(6, 0, 0, 255))).toEqual(true);
+            let color3 = gradient(5, 30);
+            expect(colors.compareColor(color3, colors.createColor(59, 193, 0, 255))).toEqual(true);
+            let color4 = gradient(25, 75);
+            expect(colors.compareColor(color4, colors.createColor(0, 155, 95, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapIsland', () => {
-	    const gradient = colorMap.island({f:(x,y)=>y,min:0,max:200});
-	    let color1 = gradient(0,0);
-	    expect(colors.compareColor(color1,colors.createColor(0,0,255,255))).toEqual(true);
-            let color2 = gradient(10,200);
-	    expect(colors.compareColor(color2,colors.createColor(128,128,0,255))).toEqual(true);
-	    let color3 = gradient(5,100);
-	    expect(colors.compareColor(color3,colors.createColor(249,228,183,255))).toEqual(true);
-	    let color4 = gradient(25,250);
-	    expect(colors.compareColor(color4,colors.createColor(135,206,235,255))).toEqual(true); 
-	});
+        test('Test of colorMapIsland', () => {
+            const gradient = colorMap.island({ f: (x, y) => y, min: 0, max: 200 });
+            let color1 = gradient(0, 0);
+            expect(colors.compareColor(color1, colors.createColor(0, 0, 255, 255))).toEqual(true);
+            let color2 = gradient(10, 200);
+            expect(colors.compareColor(color2, colors.createColor(128, 128, 0, 255))).toEqual(true);
+            let color3 = gradient(5, 100);
+            expect(colors.compareColor(color3, colors.createColor(249, 228, 183, 255))).toEqual(true);
+            let color4 = gradient(25, 250);
+            expect(colors.compareColor(color4, colors.createColor(135, 206, 235, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapIslandD', () => {
-	    const gradient = colorMap.islandD({f:(x,y)=>y,min:-100,max:400});
-	    let color1 = gradient(0,-100);
-	    expect(colors.compareColor(color1,colors.createColor(0,0,255,255))).toEqual(true);
-            let color2 = gradient(10,400);
-	    expect(colors.compareColor(color2,colors.createColor(128,128,0,255))).toEqual(true);
-	    let color3 = gradient(5,150);
-	    expect(colors.compareColor(color3,colors.createColor(130,254,5,255))).toEqual(true);
-	    let color4 = gradient(25,-200);
-	    expect(colors.compareColor(color4,colors.createColor(132,142,9,255))).toEqual(true); 
-	});
+        test('Test of colorMapIslandD', () => {
+            const gradient = colorMap.islandD({ f: (x, y) => y, min: -100, max: 400 });
+            let color1 = gradient(0, -100);
+            expect(colors.compareColor(color1, colors.createColor(0, 0, 255, 255))).toEqual(true);
+            let color2 = gradient(10, 400);
+            expect(colors.compareColor(color2, colors.createColor(128, 128, 0, 255))).toEqual(true);
+            let color3 = gradient(5, 150);
+            expect(colors.compareColor(color3, colors.createColor(130, 254, 5, 255))).toEqual(true);
+            let color4 = gradient(25, -200);
+            expect(colors.compareColor(color4, colors.createColor(132, 142, 9, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapSnow', () => {
-	    const gradient = colorMap.snow({f:(x,y)=>y,min:-50,max:50});
-	    let color1 = gradient(0,-50);
-	    expect(colors.compareColor(color1,colors.createColor(0,0,255,255))).toEqual(true);
-            let color2 = gradient(10,50);
-	    expect(colors.compareColor(color2,colors.createColor(115,0,255,0))).toEqual(true);
-	    let color3 = gradient(5,0);
-	    expect(colors.compareColor(color3,colors.createColor(117,0,255,2))).toEqual(true);
-	    let color4 = gradient(25,75);
-	    expect(colors.compareColor(color4,colors.createColor(0,118,255,121))).toEqual(true); 
-	});
+        test('Test of colorMapSnow', () => {
+            const gradient = colorMap.snow({ f: (x, y) => y, min: -50, max: 50 });
+            let color1 = gradient(0, -50);
+            expect(colors.compareColor(color1, colors.createColor(0, 0, 255, 255))).toEqual(true);
+            let color2 = gradient(10, 50);
+            expect(colors.compareColor(color2, colors.createColor(115, 0, 255, 0))).toEqual(true);
+            let color3 = gradient(5, 0);
+            expect(colors.compareColor(color3, colors.createColor(117, 0, 255, 2))).toEqual(true);
+            let color4 = gradient(25, 75);
+            expect(colors.compareColor(color4, colors.createColor(0, 118, 255, 121))).toEqual(true);
+        });
 
-	test('Test of colorMapPurple', () => {
-	    const gradient = colorMap.purple({f:(x,y)=>y,min:0,max:50});
-	    let color1 = gradient(0,0);
-	    expect(colors.compareColor(color1,colors.createColor(0,0,255,255))).toEqual(true);
-            let color2 = gradient(10,50);
-	    expect(colors.compareColor(color2,colors.createColor(115,0,0,255))).toEqual(true);
-	    let color3 = gradient(5,25);
-	    expect(colors.compareColor(color3,colors.createColor(117,117,2,255))).toEqual(true);
-	    let color4 = gradient(25,62);
-	    expect(colors.compareColor(color4,colors.createColor(0,0,126,255))).toEqual(true); 
-	});
+        test('Test of colorMapPurple', () => {
+            const gradient = colorMap.purple({ f: (x, y) => y, min: 0, max: 50 });
+            let color1 = gradient(0, 0);
+            expect(colors.compareColor(color1, colors.createColor(0, 0, 255, 255))).toEqual(true);
+            let color2 = gradient(10, 50);
+            expect(colors.compareColor(color2, colors.createColor(115, 0, 0, 255))).toEqual(true);
+            let color3 = gradient(5, 25);
+            expect(colors.compareColor(color3, colors.createColor(117, 117, 2, 255))).toEqual(true);
+            let color4 = gradient(25, 62);
+            expect(colors.compareColor(color4, colors.createColor(0, 0, 126, 255))).toEqual(true);
+        });
 
-	test('Test of colorMapHot', () => {
-	    const gradient = colorMap.hot({f:(x,y)=>x,min:10,max:40});
-	    let color1 = gradient(10,0);
-	    expect(colors.compareColor(color1,colors.createColor(28,8,1,255))).toEqual(true);
-            let color2 = gradient(40,50);
-	    expect(colors.compareColor(color2,colors.createColor(250,251,255,255))).toEqual(true);
-	    let color3 = gradient(25,3);
-	    expect(colors.compareColor(color3,colors.createColor(113,58,152,255))).toEqual(true);
-	    let color4 = gradient(47.5,47.5);
-	    expect(colors.compareColor(color4,colors.createColor(75,176,22,255))).toEqual(true); 
-	});
-    }); 
+        test('Test of colorMapHot', () => {
+            const gradient = colorMap.hot({ f: (x, y) => x, min: 10, max: 40 });
+            let color1 = gradient(10, 0);
+            expect(colors.compareColor(color1, colors.createColor(28, 8, 1, 255))).toEqual(true);
+            let color2 = gradient(40, 50);
+            expect(colors.compareColor(color2, colors.createColor(250, 251, 255, 255))).toEqual(true);
+            let color3 = gradient(25, 3);
+            expect(colors.compareColor(color3, colors.createColor(113, 58, 152, 255))).toEqual(true);
+            let color4 = gradient(47.5, 47.5);
+            expect(colors.compareColor(color4, colors.createColor(75, 176, 22, 255))).toEqual(true);
+        });
+    });
 }
 
 testColorMapExamples();
--- src/test/colorMapPredicate.test.js
+++ src/test/colorMapPredicate.test.js
@@ -3,89 +3,86 @@ const colors = require('./../colors.js');
 
 function testColorMapPredicate() {
     describe('ColorMapPredicate tests', () => {
-	let epsilon = 5
+        let epsilon = 5
         test('Test of wave', () => {
-	    expect(colorMap.wave(0,0)).toBeCloseTo(0,epsilon);
-	    expect(colorMap.wave(2,0)).toBeCloseTo(0.2*Math.sin(2),epsilon);
-	    expect(colorMap.wave(3,4)).toBeCloseTo(0.5*Math.sin(19),epsilon);
-	});
+            expect(colorMap.wave(0, 0)).toBeCloseTo(0, epsilon);
+            expect(colorMap.wave(2, 0)).toBeCloseTo(0.2 * Math.sin(2), epsilon);
+            expect(colorMap.wave(3, 4)).toBeCloseTo(0.5 * Math.sin(19), epsilon);
+        });
 
-	test('Test of splash', () => {
-	    expect(colorMap.splash(0,0)).toBeCloseTo(0,epsilon);
-	    expect(colorMap.splash(10,0)).toBeCloseTo(0,epsilon);
-	    expect(colorMap.splash(10,10)).toBeCloseTo(Math.sin(2)*10**5,epsilon);
-	});
+        test('Test of splash', () => {
+            expect(colorMap.splash(0, 0)).toBeCloseTo(0, epsilon);
+            expect(colorMap.splash(10, 0)).toBeCloseTo(0, epsilon);
+            expect(colorMap.splash(10, 10)).toBeCloseTo(Math.sin(2) * 10 ** 5, epsilon);
+        });
 
-	test('Test of pic', () => {
-	    expect(colorMap.pic(0,0)).toBeCloseTo(0,epsilon);
-	    expect(colorMap.pic(1,0)).toBeCloseTo(Math.sin(1)/1.1+0.5,epsilon);
-	    expect(colorMap.pic(10,10)).toBeCloseTo(Math.sin(400)/(0.1+Math.sqrt(200))+300*Math.exp(-199),epsilon);
-	});
+        test('Test of pic', () => {
+            expect(colorMap.pic(0, 0)).toBeCloseTo(0, epsilon);
+            expect(colorMap.pic(1, 0)).toBeCloseTo(Math.sin(1) / 1.1 + 0.5, epsilon);
+            expect(colorMap.pic(10, 10)).toBeCloseTo(Math.sin(400) / (0.1 + Math.sqrt(200)) + 300 * Math.exp(-199), epsilon);
+        });
 
-	test('Test of mandelBrotSet', () => {
-	    expect(colorMap.mandelbrotSet(50,2)(0,0)).toBeCloseTo(0,epsilon);
-	    expect(colorMap.mandelbrotSet(2,2)(0.1,0)).toBeCloseTo(Math.sqrt((0.1+0.1**2)**2),epsilon);
-	    expect(colorMap.mandelbrotSet(25,6)(25,1)).toBeCloseTo(Math.sqrt(626),epsilon);
-	});
+        test('Test of mandelBrotSet', () => {
+            expect(colorMap.mandelbrotSet(50, 2)(0, 0)).toBeCloseTo(0, epsilon);
+            expect(colorMap.mandelbrotSet(2, 2)(0.1, 0)).toBeCloseTo(Math.sqrt((0.1 + 0.1 ** 2) ** 2), epsilon);
+            expect(colorMap.mandelbrotSet(25, 6)(25, 1)).toBeCloseTo(Math.sqrt(626), epsilon);
+        });
 
-	test('Test of JuliaSquare', () => {
-	    expect(colorMap.juliaShapes.juliaSquare(1,2)(0,0)).toBeCloseTo(Math.sqrt(0.3**2+0.5**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaSquare(2,2)(20,30)).toBeCloseTo(Math.sqrt(20*20+30*30),epsilon);
-	    
-	});
+        test('Test of JuliaSquare', () => {
+            expect(colorMap.juliaShapes.juliaSquare(1, 2)(0, 0)).toBeCloseTo(Math.sqrt(0.3 ** 2 + 0.5 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaSquare(2, 2)(20, 30)).toBeCloseTo(Math.sqrt(20 * 20 + 30 * 30), epsilon);
 
-	test('Test of JuliaSpi', () => {
-	    expect(colorMap.juliaShapes.juliaSpi(1,2)(0,0)).toBeCloseTo(Math.sqrt(0.285**2+0.01**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaSpi(2,2)(100,0)).toBeCloseTo(100,epsilon);
-	    
-	});
+        });
 
-	test('Test of JuliaPeak', () => {
-	    expect(colorMap.juliaShapes.juliaPeak(1,2)(0,0)).toBeCloseTo(Math.sqrt(1.4107**2+0.0099**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaPeak(2,2)(10,20)).toBeCloseTo(Math.sqrt(100+20**2),epsilon);
-	    
-	});
+        test('Test of JuliaSpi', () => {
+            expect(colorMap.juliaShapes.juliaSpi(1, 2)(0, 0)).toBeCloseTo(Math.sqrt(0.285 ** 2 + 0.01 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaSpi(2, 2)(100, 0)).toBeCloseTo(100, epsilon);
 
-	test('Test of JuliaElec', () => {
-	    expect(colorMap.juliaShapes.juliaElec(1,2)(0,0)).toBeCloseTo(Math.sqrt(0.038**2+0.9754**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaElec(50,2)(18,13)).toBeCloseTo(Math.sqrt(18**2+13**2),epsilon);
-	    
-	});
+        });
 
-	test('Test of JuliaCrown', () => {
-	    expect(colorMap.juliaShapes.juliaCrown(1,6)(0,0)).toBeCloseTo(Math.sqrt(1.476**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaCrown(5,1)(5,6)).toBeCloseTo(Math.sqrt(61),epsilon);
-	    
-	});
+        test('Test of JuliaPeak', () => {
+            expect(colorMap.juliaShapes.juliaPeak(1, 2)(0, 0)).toBeCloseTo(Math.sqrt(1.4107 ** 2 + 0.0099 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaPeak(2, 2)(10, 20)).toBeCloseTo(Math.sqrt(100 + 20 ** 2), epsilon);
 
-	test('Test of JuliaBubble', () => {
-	    expect(colorMap.juliaShapes.juliaBubble(1,1)(0,0)).toBeCloseTo(Math.sqrt(0.4**2+0.6**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaBubble(5,15)(56,0)).toBeCloseTo(Math.sqrt(56**2),epsilon);
-	    
-	});
+        });
 
-	test('Test of JuliaDragon', () => {
-	    expect(colorMap.juliaShapes.juliaDragon(1,2)(0,0)).toBeCloseTo(Math.sqrt(0.8**2+0.156**2),epsilon);
-	    expect(colorMap.juliaShapes.juliaDragon(2,2)(10,0)).toBeCloseTo(10,epsilon);
-	    
-	});
+        test('Test of JuliaElec', () => {
+            expect(colorMap.juliaShapes.juliaElec(1, 2)(0, 0)).toBeCloseTo(Math.sqrt(0.038 ** 2 + 0.9754 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaElec(50, 2)(18, 13)).toBeCloseTo(Math.sqrt(18 ** 2 + 13 ** 2), epsilon);
 
-	test('Test of fractale', () => {
-	    expect(colorMap.fractale(50,2,(z)=>{return [z[0]**2,0];})(0,0)).toBeCloseTo(0,epsilon);
-	    expect(colorMap.fractale(3,2,(z)=>{return [z[0]**2,0];})(0.5,0)).toBeCloseTo(0.00390625,epsilon);
-	    expect(colorMap.fractale(5,6,(z)=>{return [z[0]**3,0];})(10,0)).toBeCloseTo(10,epsilon);
-	});
+        });
 
-	test('Test of focused', () => {
-	    const f = (x,y)=>{return x+y ;};
-	    expect(colorMap.focused(f,250,250,-10,10,-5,5)(0,125)).toBeCloseTo(f(-10,0),epsilon);
-	    expect(colorMap.focused(f,250,250,-10,0,-5,5)(250,0)).toBeCloseTo(f(0,-5),epsilon);
-	    expect(colorMap.focused(f,50,100,-22,22,-50,-10)(50,50)).toBeCloseTo(f(0,-10),epsilon);    
-	});
-	
+        test('Test of JuliaCrown', () => {
+            expect(colorMap.juliaShapes.juliaCrown(1, 6)(0, 0)).toBeCloseTo(Math.sqrt(1.476 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaCrown(5, 1)(5, 6)).toBeCloseTo(Math.sqrt(61), epsilon);
 
-	
-    }); 
+        });
+
+        test('Test of JuliaBubble', () => {
+            expect(colorMap.juliaShapes.juliaBubble(1, 1)(0, 0)).toBeCloseTo(Math.sqrt(0.4 ** 2 + 0.6 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaBubble(5, 15)(56, 0)).toBeCloseTo(Math.sqrt(56 ** 2), epsilon);
+
+        });
+
+        test('Test of JuliaDragon', () => {
+            expect(colorMap.juliaShapes.juliaDragon(1, 2)(0, 0)).toBeCloseTo(Math.sqrt(0.8 ** 2 + 0.156 ** 2), epsilon);
+            expect(colorMap.juliaShapes.juliaDragon(2, 2)(10, 0)).toBeCloseTo(10, epsilon);
+
+        });
+
+        test('Test of fractale', () => {
+            expect(colorMap.fractale(50, 2, (z) => { return [z[0] ** 2, 0]; })(0, 0)).toBeCloseTo(0, epsilon);
+            expect(colorMap.fractale(3, 2, (z) => { return [z[0] ** 2, 0]; })(0.5, 0)).toBeCloseTo(0.00390625, epsilon);
+            expect(colorMap.fractale(5, 6, (z) => { return [z[0] ** 3, 0]; })(10, 0)).toBeCloseTo(10, epsilon);
+        });
+
+        test('Test of focused', () => {
+            const f = (x, y) => { return x + y; };
+            expect(colorMap.focused(f, 250, 250, -10, 10, -5, 5)(0, 125)).toBeCloseTo(f(-10, 0), epsilon);
+            expect(colorMap.focused(f, 250, 250, -10, 0, -5, 5)(250, 0)).toBeCloseTo(f(0, -5), epsilon);
+            expect(colorMap.focused(f, 50, 100, -22, 22, -50, -10)(50, 50)).toBeCloseTo(f(0, -10), epsilon);
+        });
+    });
 }
 
 testColorMapPredicate();
